c4fc5cd4d8df4cb57aa4febeb6cb78d01f0c6bd4
Daniel Hannon <hello111f@gmail.com>
2025-04-08 20:34:43 +0100

    some git crap

--- lua/danhan/plugins/snacks-nvim.lua
+++ lua/danhan/plugins/snacks-nvim.lua
@@ -110,20 +110,27 @@ return {
 				{ section = "keys", gap = 1, padding = 1 },
 				{ pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
 				{ pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-				{
-					pane = 2,
-					icon = " ",
-					title = "Git Status",
-					section = "terminal",
-					enabled = function()
-						return Snacks.git.get_root() ~= nil
-					end,
-					cmd = "git --no-pager diff --stat -B -M -C",
-					height = 12,
-					padding = 1,
-					ttl = 5 * 60,
-					indent = 3,
-				},
+				function()
+					local in_git = Snacks.git.get_root() ~= nil
+					local cmds = {
+						{
+							icon = " ",
+							title = "Git Status",
+							cmd = "git --no-pager diff --stat -B -M -C",
+							height = 12,
+						},
+					}
+					return vim.tbl_map(function(cmd)
+						return vim.tbl_extend("force", {
+							pane = 2,
+							section = "terminal",
+							enabled = in_git,
+							padding = 1,
+							ttl = 5 * 60,
+							indent = 3,
+						}, cmd)
+					end, cmds)
+				end,
 				{ section = "startup" },
 			},
 		},
