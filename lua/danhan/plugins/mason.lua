0eedcc8982953da91fdbecced467d0330cec0092
Daniel Hannon <hello111f@gmail.com>
2026-02-14 21:59:55 +0000

    Updated for new lazyvim version to prevent some breakages

--- lua/danhan/plugins/mason.lua
+++ lua/danhan/plugins/mason.lua
@@ -1,5 +1,5 @@
 return {
-	"williamboman/mason.nvim",
+	"mason-org/mason.nvim",
 	opts = function(_, opts)
 		opts.ui = {
 			border = "rounded",
--- lua/danhan/plugins/treesitter.lua
+++ lua/danhan/plugins/treesitter.lua
@@ -4,18 +4,6 @@ return {
 	cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "c", "lua", "cpp", "go" },
-
-			sync_install = false,
-			auto_install = true,
-
-			highlight = {
-				enable = true,
-				additional_vim_highlighting = false,
-			},
-
-			indent = { enable = true },
-		})
+		require("nvim-treesitter").install({ "c++", "go" })
 	end,
 }
